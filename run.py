1854d0695f30a1933ff8daff9a0bbefc8affe0e5
Vadim Markovtsev <vadim@sourced.tech>
2019-03-01 10:01:41 +0100

    Polish the timeline
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- run.py
+++ run.py
@@ -2,12 +2,10 @@ from contextlib import contextmanager
 import logging
 import time
 
-import coloredlogs
-from matplotlib import pyplot
-import numpy
 import pymysql
 
 logger = logging.getLogger("run")
+
 
 @contextmanager
 def no_logs():
@@ -18,8 +16,10 @@ def no_logs():
     finally:
         logger.setLevel(level)
 
-def run(query, host="localhost", user="root", passwd=""):
-    conn = pymysql.connect(host=host, port=3306, user=user, passwd=passwd, db="mysql")
+
+def run(query, host="localhost", port=3306, user="root", passwd=""):
+    """Query gitbase. Return a tuple of the resulting SQL table columns."""
+    conn = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db="mysql")
     
     start = time.time()
     cursor = conn.cursor()
@@ -27,10 +27,15 @@ def run(query, host="localhost", user="root", passwd=""):
     end = time.time()
     
     logger.info("done in %.2f seconds" % (end - start))
-    return cursor
+    result = tuple(zip(*cursor))
+    if len(result) == 1:
+        return result[0]
+    return result
+
 
 def run_one(query, log=True):
     return run(query, log).fetchone()
+
 
 def run_and_print(query):
     for row in run(query):
